b5aee0f5bbfd74ccdf0a3d27aad86af3c2c35b4c
Isabella <sorianoisabella28@gmail.com>
2023-10-16 17:11:07 -0500

    implementar el widget(vista) de pokemon_lis

---
+++ lib/screens/pokemon_list.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter/material.dart';
+
+class PokemonList extends StatefulWidget{
+
+  const PokemonList({super.key, required this.pokemons});
+  final List pokemons;
+
+  @override
+  State<PokemonList> createState()=> _PokemonListState();
+}
+
+class _PokemonListState extends State<PokemonList>{
+
+  @override
+  Widget build(BuildContext context){
+
+    return ListView.builder(
+
+      itemCount: widget.pokemons.length,
+
+      itemBuilder: (context,index){
+        return ListTile(
+          title: Text(widget.pokemons[index].name),
+        );
+      }
+    );
+  }
+}
+
+
