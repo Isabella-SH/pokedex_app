0a326384ae5c5a9b63be35fa8b19c92696c9eb70
Isabella <sorianoisabella28@gmail.com>
2023-10-16 17:42:45 -0500

    primera version de mostrar el widget, aun no funciona

--- lib/screens/pokemon_list.dart
+++ lib/screens/pokemon_list.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class PokemonList extends StatefulWidget{
 
   const PokemonList({super.key, required this.pokemons});
-  final List pokemons;
+  final List? pokemons;
 
   @override
   State<PokemonList> createState()=> _PokemonListState();
@@ -17,11 +17,11 @@ class _PokemonListState extends State<PokemonList>{
 
     return ListView.builder(
 
-      itemCount: widget.pokemons.length,
+      itemCount: (widget.pokemons==null)? 0: widget.pokemons?.length,
 
       itemBuilder: (context,index){
         return ListTile(
-          title: Text(widget.pokemons[index].name),
+          title: Text(widget.pokemons?[index].name),
         );
       }
     );
