2822fa75841280ee3afe3e1a1324f9ab9498003d
Isabella <sorianoisabella28@gmail.com>
2023-10-21 20:17:00 -0500

    implementar pokemon_database.dart

---
+++ lib/databases/pokemon_database.dart
@@ -0,0 +1,28 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class PokemonDatabase{
+
+  final int version=1;
+  final String databaseName="Pokedex.db";
+  final String tableName="pokemons";
+
+  Database? db;
+
+  //funcion patra la creacion de nuetsra base de datos
+  //y la tabla llamada pokemons que almacena el id
+  Future<Database> openDb()async{
+    db ??= await openDatabase(join(await getDatabasesPath(), databaseName),
+      onCreate: (database, version){
+      database.execute("create table $tableName (id integer primary key)");
+      }, version: version
+    );
+    return db as Database;
+  }
+}
+
+
+
+
+
+
